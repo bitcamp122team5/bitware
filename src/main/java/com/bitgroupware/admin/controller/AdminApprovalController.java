a8f3c0ef41c0711353af1b46b900f999a37f7469
hansol <hs_1022@naver.com>
2019-08-27 20:19:25 +0900

    Admin - Approval AdminApprovalController.java approvalDocWrite.html 글 쓰기 - 첨부파일 추가

--- src/main/java/com/bitgroupware/admin/controller/AdminApprovalController.java
+++ src/main/java/com/bitgroupware/admin/controller/AdminApprovalController.java
@@ -1,5 +1,6 @@
 package com.bitgroupware.admin.controller;
 
+import java.io.File;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +9,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bitgroupware.approval.beans.ApprovalDoucemtDto;
+import com.bitgroupware.approval.beans.ApprovalFileDto;
 import com.bitgroupware.approval.service.ApprovalService;
 
-/**
- * @Title	ApprovalController
- * @Author 	홍한솔
- * @Date	2019. 8. 20.
- */
 
 @Controller
 @RequestMapping("/admin")
@@ -24,39 +21,56 @@ public class AdminApprovalController {
 	private ApprovalService approvalService;
 	
 	// 문서리스트
-	@RequestMapping("/selectApprovalDocList")
-	public String selectApprovalDocList(Model model) {
-		
-		List<ApprovalDoucemtDto> approvalDocList = approvalService.selectApprovalDocList();
-		model.addAttribute("approvalDocList",approvalDocList);
-		
-		return "admin/approval/approvalDocList";
-	}
-	
-	// 등록페이지(insert+update)
-	@RequestMapping("/insertApprovalDocView")
-	public String insertApprovalDocView(Model model,ApprovalDoucemtDto apdocDto) {
-		
-		if(apdocDto.getApdocNo() != null) { // 수정할 때 필요 해서  Dto 가져감
-			apdocDto = approvalService.selectApprovalDoc(apdocDto.getApdocNo());
-			model.addAttribute("apdocDto",apdocDto);
+		@RequestMapping("/selectApprovalDocList")
+		public String selectApprovalDocList(Model model) {
+			List<ApprovalDoucemtDto> approvalDocList = approvalService.selectApprovalDocList();
+			model.addAttribute("approvalDocList",approvalDocList);
+			return "admin/approval/approvalDocList";
 		}
-		return "admin/approval/approvalDocWrite";
-	}
-	
-	
-	// 등록(insert+update)
-	@RequestMapping("/insertApprovalDoc")
-	public String insertApprovalDoc(Model model,ApprovalDoucemtDto apdocDto) {
-		approvalService.insertApprovalDoc(apdocDto);
-		return "redirect:/admin/selectApprovalDocList";
-	}
-	
-	
-	// 삭제
-	@RequestMapping("/deleteApprovalDoc")
-	public String deleteApprovalDocList(Model model,ApprovalDoucemtDto apdocDto) {
-		approvalService.deleteApprovalDoc(apdocDto);
-		return "redirect:/admin/selectApprovalDocList";
-	}
+		
+		// 등록페이지(insert+update)
+		@RequestMapping("/insertApprovalDocView")
+		public String insertApprovalDocView(Model model,ApprovalDoucemtDto apdocDto) {
+			if(apdocDto.getApdocNo() != null) { // 수정할 때 필요 해서  Dto 가져감
+				apdocDto = approvalService.selectApprovalDoc(apdocDto.getApdocNo());
+				model.addAttribute("apdocDto",apdocDto);
+			}
+			return "admin/approval/approvalDocWrite";
+		}
+		
+		
+		// 등록(insert+update)
+		@RequestMapping("/insertApprovalDoc")
+		public String insertApprovalDoc(Model model, ApprovalDoucemtDto apdocDto,ApprovalFileDto apfileDto) {
+			
+			
+//			if (!file.getOriginalFilename().isEmpty()) {
+//				String fileName = apdocFileDto.getFile().getOriginalFilename();
+//				String path = request.getServletContext().getRealPath("upload");
+//				apdocFileDto.getFile().transferTo(new File(path+fileName));
+//				apdocFileDto.setApFileUrl(fileName);
+//			}
+			String apFileName = "Empty";
+			
+			if (!apfileDto.getFile().isEmpty()) {
+				apFileName = apfileDto.getFile().getOriginalFilename();
+				try {
+					String path = UPLOAD_DIR;
+					System.out.println("path"+path);
+					apfileDto.getFile().transferTo(new File(path + apFileName));
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+			apfileDto.setApFileUrl(apFileName);
+			approvalService.insertApprovalDoc(apdocDto);
+			return "redirect:/admin/selectApprovalDocList";
+		}
+		
+		// 삭제
+		@RequestMapping("/deleteApprovalDoc")
+		public String deleteApprovalDocList(Model model,ApprovalDoucemtDto apdocDto) {
+			approvalService.deleteApprovalDoc(apdocDto);
+			return "redirect:/admin/selectApprovalDocList";
+		}
 }
