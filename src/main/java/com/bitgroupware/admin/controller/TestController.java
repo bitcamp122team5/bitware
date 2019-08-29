11adcddbcf86eab4bb2fd0b6bdd26ce0772fcb2f
JaebeomKwon <jaebumi0415@naver.com>
2019-08-29 09:50:07 +0900

    멥 중간 커밋

--- src/main/java/com/bitgroupware/admin/controller/TestController.java
+++ src/main/java/com/bitgroupware/admin/controller/TestController.java
@@ -1,11 +1,8 @@
 package com.bitgroupware.admin.controller;
 
-import java.util.Date;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -314,6 +311,15 @@ public class TestController {
 		memberRepo.save(member);
 		
 		return "PL 하나 생성완료";
+	}
+	
+	@RequestMapping("/map")
+	public String map() {
+		return "map";
+	}
+	@RequestMapping("/mapmap")
+	public String mapmap() {
+		return "mapmap";
 	}
 	
 }
