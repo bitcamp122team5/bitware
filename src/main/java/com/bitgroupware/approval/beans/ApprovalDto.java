46af17d223269a1e2da2b6094cb84eb9b72c1883
hansol <hs_1022@naver.com>
2019-09-03 12:40:50 +0900

    Admin - 파일 첨부 기능 빼기 User - 결재 부분( 사용자가 보는 경로 매핑 )수정

--- src/main/java/com/bitgroupware/approval/beans/ApprovalDto.java
+++ src/main/java/com/bitgroupware/approval/beans/ApprovalDto.java
@@ -10,12 +10,12 @@ import lombok.ToString;
 @Setter
 @ToString
 public class ApprovalDto {
-	private String apNo;			// 문서번호
+	private int apNo;			// 문서번호
 	private String apTitle;			// 제목
 	private String apContent;		// 문서내용
 	private String apDeleteflag;	// 삭제여부
 	private String apDocstatus;		// 문서상태
-	private String apdocNo;			// 문서양식번호
+	private int apdocNo;			// 문서양식번호
 	private Date apInsertdate;	// 작성일자
 	private String memId;			// 사용자번호
 	private String apSignpath;		// 다음결제자
