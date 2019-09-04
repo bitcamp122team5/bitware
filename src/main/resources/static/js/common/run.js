1f766a7c71811003f888ee721596b9120d7715be
junwoo <wnsdn5000@naver.com>
2019-09-04 18:53:04 +0900

    레이아웃 수정

--- src/main/resources/static/js/common/run.js
+++ src/main/resources/static/js/common/run.js
@@ -4,3 +4,28 @@ $(function(){
 		$(this).toggleClass("on");
 	});
 });
+
+/* 채팅 아코디언  */
+$(function(){
+	$(".chat_wrap button").siblings(".chat_inner").css("width","0px");
+	
+	$(".chat_wrap button").click(function(){
+		$(".chat_wrap button").toggleClass("on");
+		if($(".chat_wrap button").hasClass("on") == true){
+			$(".chat_wrap button").siblings(".chat_inner").animate({"width":"400px"});
+		}else{
+			$(".chat_wrap button").siblings(".chat_inner").animate({"width":"0px"});
+		}
+	});
+});
+
+//tab 
+$(function(){   
+    $(".tab > ul li").click(function(){
+        var now_tab = $(this).index();
+        $(this).parent().find("li").removeClass("on");
+        $(this).parent().parent().parent().find(".info").addClass("hidden");
+        $(this).parent().find("li").eq(now_tab).addClass("on");
+        $(this).parent().parent().parent().find(".info").eq(now_tab).removeClass("hidden");
+    });
+});
