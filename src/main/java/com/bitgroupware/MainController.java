ec590a79e7aff23d39c63aa4b48a5d68f005caed
DongwonJung <tiero333@gmail.com>
2019-09-20 20:42:53 +0900

    메인 - 프로젝트 차트 드디어 적용 css적인 부분 수정 필요

--- src/main/java/com/bitgroupware/MainController.java
+++ src/main/java/com/bitgroupware/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.bitgroupware.approval.beans.ApprovalDto;
 import com.bitgroupware.approval.persistence.ApprovalDao;
@@ -64,53 +65,9 @@ public class MainController {
 			// 참여중인 프로젝트
 			List<ProjectInfoDto> projectInfoList = projectDao.selectMainAttendProjectList(principal.getMember().getMemId());
 			List<MainViewProjectList> projectList = new ArrayList<MainViewProjectList>();
-			int curdate = Integer.parseInt(commuteRepository.selectCurdate().replace("-", ""));
 			for (ProjectInfoDto projectInfo : projectInfoList) {
-				int planComplete = 0;
-				int planIng = 0;
-				int planUnset = 0;
-				int realComplete = 0;
-				int realIng = 0;
-				int realUnset = 0;
 				MainViewProjectList mainViewProject = new MainViewProjectList();
-				for(ProjectWbsDto projectWbs : projectDao.selectProjectWbsList(projectInfo.getPrjCode())) {
-					/*
-					 계획 작업 종료일 < 오늘 -> 작업 완료
-					 계획 작업 시작일 <= 오늘 -> 작업 진행
-					 계획 작업 시작일 > 오늘 -> 작업 미시작
-					 */
-					if (Integer.parseInt(projectWbs.getPrjPlanEnd().replace("-", "")) < curdate) planComplete++;
-					else if (Integer.parseInt(projectWbs.getPrjPlanStart().replace("-", "")) <= curdate) planIng++;
-					else planUnset++;
-					/*
-					실제 작업 시작일 입력 / 실제 작업 종료일(오늘보다 전) 입력 -> 작업 완료
-					실제 작업 시작일 입력 / 실제 작업 종료일(오늘보다 후or오늘) 입력 -> 작업 진행
-					
-					실제 작업 시작일(오늘보다 전or오늘) 입력 / 실제 작업 종료일 미입력 - > 작업 진행
-					실제 작업 시작일(오늘보다 후) 입력 / 실제 작업 종료일 미입력 -> 작업 미시작
-					실제 작업 시작일 미입력 / 실제 작업 종료일 미입력 -> 작업 미시작
-					 */
-					try {
-						if (Integer.parseInt(projectWbs.getPrjRealEnd().replace("-", "")) < curdate) realComplete++;
-						else realIng++;
-					} catch (NullPointerException realEndIsNull) {
-						System.out.println("RealEnd가 입력되지 않음");
-						try {
-							if (Integer.parseInt(projectWbs.getPrjRealStart().replace("-", "")) <= curdate) realIng++;
-							else realUnset++;
-						} catch (NullPointerException realStartIsNull) {
-							System.out.println("RealStart가 입력되지않음");
-							realUnset++;
-						}
-					}
-				}
 				mainViewProject.setProjectInfo(projectInfo);
-				mainViewProject.setPlanComplete(planComplete);
-				mainViewProject.setPlanIng(planIng);
-				mainViewProject.setPlanUnset(planUnset);
-				mainViewProject.setRealComplete(realComplete);
-				mainViewProject.setRealIng(realIng);
-				mainViewProject.setRealUnset(realUnset);
 				projectList.add(mainViewProject);
 			}
 			
@@ -138,6 +95,64 @@ public class MainController {
 			
 			return "index";
 		}
+	}
+	
+	@RequestMapping("/calcWorks")
+	@ResponseBody
+	public MainViewProjectList arrayTest(@AuthenticationPrincipal SecurityUser principal, int prjListNum) {
+		List<ProjectInfoDto> projectInfoList = projectDao.selectMainAttendProjectList(principal.getMember().getMemId());
+		
+		int curdate = Integer.parseInt(commuteRepository.selectCurdate().replace("-", ""));
+		
+		int planComplete = 0;
+		int planIng = 0;
+		int planUnset = 0;
+		int realComplete = 0;
+		int realIng = 0;
+		int realUnset = 0;
+		
+		for(ProjectWbsDto projectWbs : projectDao.selectProjectWbsList(projectInfoList.get(prjListNum).getPrjCode())) {
+			/*
+			 계획 작업 종료일 < 오늘 -> 작업 완료
+			 계획 작업 시작일 <= 오늘 -> 작업 진행
+			 계획 작업 시작일 > 오늘 -> 작업 미시작
+			 */
+			if (Integer.parseInt(projectWbs.getPrjPlanEnd().replace("-", "")) < curdate) planComplete++;
+			else if (Integer.parseInt(projectWbs.getPrjPlanStart().replace("-", "")) <= curdate) planIng++;
+			else planUnset++;
+			/*
+			실제 작업 시작일 입력 / 실제 작업 종료일(오늘보다 전) 입력 -> 작업 완료
+			실제 작업 시작일 입력 / 실제 작업 종료일(오늘보다 후or오늘) 입력 -> 작업 진행
+			
+			실제 작업 시작일(오늘보다 전or오늘) 입력 / 실제 작업 종료일 미입력 - > 작업 진행
+			실제 작업 시작일(오늘보다 후) 입력 / 실제 작업 종료일 미입력 -> 작업 미시작
+			실제 작업 시작일 미입력 / 실제 작업 종료일 미입력 -> 작업 미시작
+			 */
+			try {
+				if (Integer.parseInt(projectWbs.getPrjRealEnd().replace("-", "")) < curdate) realComplete++;
+				else realIng++;
+			} catch (NullPointerException realEndIsNull) {
+				System.out.println("RealEnd가 입력되지 않음");
+				try {
+					if (Integer.parseInt(projectWbs.getPrjRealStart().replace("-", "")) <= curdate) realIng++;
+					else realUnset++;
+				} catch (NullPointerException realStartIsNull) {
+					System.out.println("RealStart가 입력되지않음");
+					realUnset++;
+				}
+			}
+		}
+		
+		MainViewProjectList mainViewProjectList = new MainViewProjectList();
+		
+		mainViewProjectList.setPlanComplete(planComplete);
+		mainViewProjectList.setPlanIng(planIng);
+		mainViewProjectList.setPlanUnset(planUnset);
+		mainViewProjectList.setRealComplete(realComplete);
+		mainViewProjectList.setRealIng(realIng);
+		mainViewProjectList.setRealUnset(realUnset);
+		
+		return mainViewProjectList;
 	}
 
 }
